2b51333189cd84ce1e7ef19166b87d275808c1a8
Prince panwar <princegujjar1.pp@gmail.com>
2023-07-15 14:13:14 +0530

    Update abstract_contract&&Inheritance.sol

--- abstract_contract&&Inheritance.sol
+++ abstract_contract&&Inheritance.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.18;
  abstract contract addition{
    function add(uint a,uint b) external  virtual   returns (uint c);
 }
-//this function inherit abstract function and implement its funtion
+//this contract inherit abstract function and implement its funtion
 //multiple inheritance can be done like contract derived is addition , myContract{} 
 //When doing multiple inheritance the contracts namse should in be inorder frmo the leat inheriting to most inheriting 
 
